ec9437cdcc07418fd9f1111f1457ed66968e5304
Pak <cuckoong@users.noreply.github.com>
2023-01-04 11:53:59 +0800

    Add detrend signal and quality prediction example

---
+++ detrend_signal.py
@@ -0,0 +1,48 @@
+from scipy import signal
+
+
+class Custom:
+
+    def _check_params(self):
+        # check if params is a dict and has the required keys
+        if not isinstance(self.params, dict) or not all(
+                key in self.params for key in ('graph_type', 'buffer_size', 'update_interval')):
+            raise ValueError('params must be a dict and have the required keys')
+
+    def __init__(self):
+        # Params for graphs setting
+        # required keys: graph_type , update_interval
+        self.params = dict(
+            # 'line' or 'bar', required
+            graph_type='line',
+            # in ms, required
+            update_interval=100,
+            # in seconds, required
+            buffer_size=5,
+            # optional, but highly recommend to set
+            # x_range=[0, 2500],
+            # optional, but highly recommend to set
+            # y_range=[-1e-4, 1e4],
+            # optional, but highly recommend to set
+            # x_ticks=[0, 500, 1000, 1500, 2000, 2500],
+            # optional, but highly recommend to set
+            # y_ticks=[0, 500, 1000, 1500, 2000, 2500],
+            # optional, but highly recommend to set
+            x_label='Data Point',
+            # optional, but highly recommend to set
+            y_label='uV',
+            # optional, but highly recommend to set
+            title='Detrended Signal',
+        )
+
+        self._check_params()
+
+    def custom_func(self, data):
+        """
+        :param data: (1d numpy.array, or list), shape: (n, ), data feeding from GUI, n is the number of samples
+        :return: (1d numpy.array), shape: (n, ), n is the number of dimension of the output
+        """
+
+        # detrend the signal by subtracting the linear least-squares fit of data
+        detrended_signal = signal.detrend(data)
+        return detrended_signal
